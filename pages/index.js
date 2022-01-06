5977f60beba33cd5635390565e6f2beae24a7d66
JS <49812749+JS00001@users.noreply.github.com>
2022-01-05 22:43:33 -0500

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,111 @@
-import Head from 'next/head'
+import React, { useEffect } from 'react';
+import Head from 'next/head';
+import axios from 'axios';
+import {BsHeadphones, BsTelephoneXFill} from 'react-icons/bs';
+import {FaMicrophoneSlash} from 'react-icons/fa';
+
 
 export default function Home() {
+  const [disconnecting, setDisconnecting] = React.useState(false);
+  const [deafened, setDeafened] = React.useState(false);
+  const [muted, setMuted] = React.useState(false);
+
+  const [loading, setLoading] = React.useState(true);
+  const [user, setUser] = React.useState('');
+
+  const url = 'https://example.com'
+
+  const handleChange = (event) => {
+    setUser(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUser('');
+    axios.get(url + '/update/user?user=' + user);
+  }
+
+  const handleMute = () => {
+    setMuted(!muted);
+    axios.get(url + '/update/mute');
+  }
+
+  const handleDeafen = () => {
+    setDeafened(!deafened)
+    axios.get(url + '/update/deafen');
+  }
+
+  const handleDisconnect = () => {
+    setDisconnecting(!disconnecting);
+    axios.get(url + '/update/disconnect');
+  }
+
+  useEffect(() => {
+    axios.get('https://api.jsenyitko.tech/status')
+    .then(res => {
+      const {muteEnabled, deafenedEnabled, disconnectEnabled} = res.data;
+      setDisconnecting(disconnectEnabled)
+      setDeafened(deafenedEnabled);
+      setMuted(muteEnabled);
+      setLoading(false)
+    })
+  })
+
+
+  if (loading) return <h1>Loading</h1>
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-zinc-800">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Voice Manager</title>
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+        <h1 className="text-6xl font-black tracking-tight text-white font-sans">
+          Update and Manage{' '}
+        </h1>
+        <h1 className='text-6xl font-black tracking-tight text-white font-sans'>
+          Your{' '}
+          <span className='bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500'>
+            Members
+          </span>
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+        <div className="grid grid-cols-3 gap-x-5 items-stretch mt-10">
+          <div onClick={handleMute} className={`p-5 border-2 rounded-md ${muted ? 'border-blue-700' : 'border-zinc-700'} ${muted ? 'text-blue-700' : 'text-gray-400'} ${muted && 'bg-slate-800'} hover:text-blue-700 hover:bg-slate-800 hover:border-blue-700 hover:cursor-pointer`}>
+            <div className='pl-8 pb-4'> 
+              <FaMicrophoneSlash className='text-5xl'/>
+            </div>
+            <h1 className='text-l font-semibold'>
+              Auto Mute
+            </h1>
+          </div>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+          <div onClick={handleDisconnect} className={`p-5 border-2 rounded-md ${disconnecting ? 'border-blue-700' : 'border-zinc-700'} ${disconnecting ? 'text-blue-700' : 'text-gray-400'} ${disconnecting && 'bg-slate-800'} hover:text-blue-700 hover:bg-slate-800 hover:border-blue-700 hover:cursor-pointer`}>
+            <div className='pl-8 pb-4'> 
+              <BsTelephoneXFill className='text-5xl'/>
+            </div>
+            <h1 className='text-l font-semibold'>
+              Auto Disconnect
+            </h1>
+          </div>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div onClick={handleDeafen} className={`p-5 border-2 rounded-md ${deafened ? 'border-blue-700' : 'border-zinc-700'} ${deafened ? 'text-blue-700' : 'text-gray-400'} ${deafened && 'bg-slate-800'} hover:text-blue-700 hover:bg-slate-800 hover:border-blue-700 hover:cursor-pointer`}>
+            <div className='pl-8 pb-4'> 
+              <BsHeadphones className='text-5xl'/>
+            </div>
+            <h1 className='text-l font-semibold'>
+              Auto Deafen
+            </h1>
+          </div>
         </div>
+
+        <form className='pt-10 w-max' onSubmit={handleSubmit}>
+          <input className='p-2 rounded-lg w-96' type="text" onChange={handleChange} value={user}>New User ID</input> 
+          <input className='bg-blue-700 py-2 px-9 ml-5 rounded-lg text-white font-semibold hover:cursor-pointer hover:bg-blue-800' type="submit" value="Set ID"/>
+        </form>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
 }
